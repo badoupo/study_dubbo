5b401a2ea30fb7e5052b8d59d71d774f994d137e
badoupo <wuzhaoweijc@163.com>
2019-11-27 00:30:35 +0800

    build

--- study-dubbo-provider/src/main/java/com/study/badoupo/entity/User.java
+++ study-dubbo-provider/src/main/java/com/study/badoupo/entity/User.java
@@ -1,16 +1,16 @@
 package com.study.badoupo.entity;
 
-import java.time.LocalDateTime;
-
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.study.badoupo.entity.BaseEntity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
+import java.time.LocalDateTime;
+
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author badoupo
@@ -19,7 +19,7 @@ import lombok.experimental.Accessors;
 @Data
 @EqualsAndHashCode(callSuper = true)
 @Accessors(chain = true)
-@TableName("study_user")
+@TableName(value = "study_user")
 public class User extends BaseEntity {
 
     private static final long serialVersionUID = 1L;
@@ -27,51 +27,61 @@ public class User extends BaseEntity {
     /**
      * 名称
      */
+    @TableField("name")
     private String name;
 
     /**
      * 创建人
      */
+    @TableField("create_user")
     private String createUser;
 
     /**
      * 创建时间
      */
+    @TableField("create_time")
     private LocalDateTime createTime;
 
     /**
      * 更新人
      */
+    @TableField("update_user")
     private String updateUser;
 
     /**
      * 更新时间
      */
+    @TableField("update_time")
     private LocalDateTime updateTime;
 
     /**
      * 状态（0：可用，不可用）
      */
+    @TableField("status")
     private String status;
 
     /**
      * 性别（1：男，2：女）
      */
+    @TableField("sex")
     private String sex;
 
     /**
      * 删除标识（0：未删除，1：已删除）
      */
+    @TableField("deleted")
     private Boolean deleted;
 
     /**
      * 年龄
      */
+    @TableField("age")
     private Integer age;
 
     /**
      * 邮箱
      */
+    @TableField("email")
     private String email;
 
 
